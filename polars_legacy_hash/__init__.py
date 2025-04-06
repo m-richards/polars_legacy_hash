40d84cf1dfd9810922b1d2cae025625f2ce50f9a
Matt Richards <mrichards7@outlook.com.au>
2025-04-07 07:53:13 +1000

    lint

--- polars_legacy_hash/__init__.py
+++ polars_legacy_hash/__init__.py
@@ -8,7 +8,7 @@ from polars.plugins import register_plugin_function
 try:
     from polars._typing import IntoExpr
 except ImportError:
-    from polars.type_aliases import IntoExpr
+    from polars.type_aliases import IntoExpr  # type: ignore[no-redef] # noqa:I001
 
 from polars_legacy_hash._internal import __version__ as __version__
 
