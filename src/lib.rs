c9a34ee05c80eeb642a8be2bfb055ebf909a1f6f
Matt Richards <45483497+m-richards@users.noreply.github.com>
2025-04-15 21:03:52 +1000

    Ci + rust polars directly (squashed) (#2)
    * no venv in ci file?
    
    * chdir
    
    * fix deprecated versions
    
    * lint
    
    * cache builds
    
    * lint
    
    * structs suck
    
    * calling new polars methods is obviously not going to work
    
    * lint
    
    * explicit lockfile
    
    * cleanup
    
    * reenable abi3
    
    * just use old polars directly
    
    * rustfmt
    
    * try fix wheel cache
    
    * part revert
    
    * try strategy again
    
    * os
    
    * de-dupe sdists
    
    * enable releases
    
    * release won't start
    
    * indentation will help
    
    * mac back
    
    * version tests
    
    * typo
    
    * fix
    
    * dumb
    
    * old compat and readme note
    
    * fix mypy
    
    * wrong vers
    
    * mypy fix
    
    * fmt
    
    * fix
    
    * lint on latest
    
    * update min min

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,14 @@ mod expressions;
 mod pl_legacy_hashing;
 
 use pyo3::types::PyModule;
-use pyo3::{pymodule, Bound, PyResult, Python};
-use pyo3_polars::PolarsAllocator;
-
-#[global_allocator]
-static ALLOC: PolarsAllocator = PolarsAllocator::new();
+use pyo3::{pymodule, PyResult, Python};
+// use pyo3_polars::PolarsAllocator;
+//
+// #[global_allocator]
+// static ALLOC: PolarsAllocator = PolarsAllocator::new();
 
 #[pymodule]
-fn _internal(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn _internal(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add("__version__", env!("CARGO_PKG_VERSION"))?;
     Ok(())
 }
