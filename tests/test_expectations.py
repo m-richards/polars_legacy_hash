b6d9cea4711e62f816d8b48c07845ac305c960a9
Matt Richards <mrichards7@outlook.com.au>
2025-04-27 14:50:27 +1000

    ci

--- tests/test_expectations.py
+++ tests/test_expectations.py
@@ -18,10 +18,10 @@ def test_old_polars_hash(expected_int64_neg_42, expected_int32_neg_42, expected_
     actual = pl.Series("test", [42], dtype=pl.UInt32).hash()
     assert_series_equal(expected_uint32_42, actual)
 
+
 def test_int8(expected_int8_42):
     actual = pl.Series("test", [42], dtype=pl.Int8).hash()
     assert_series_equal(expected_int8_42, actual)
-
 
 
 def test_int_struct(raw_struct_df, expected_int_struct):
@@ -38,7 +38,7 @@ def test_int_struct_singular(raw_struct_df_singular, expected_int_struct_singula
     actual = raw_struct_df_singular.to_struct("test").hash()
     assert_series_equal(expected_int_struct_singular, actual)
 
+
 def test_int_dtype_struct(int_dtype_struct, expected_int_dtype_struct):
     actual = int_dtype_struct.to_struct("test").hash()
     assert_series_equal(expected_int_dtype_struct, actual)
-
--- tests/test_hash.py
+++ tests/test_hash.py
@@ -15,11 +15,11 @@ def test_oldhash_int32(expected_int32_neg_42):
     result = df.select(plh.oldhash(pl.col("test")))
     assert_frame_equal(result, expected_int32_neg_42.to_frame())
 
+
 def test_oldhash_int8(expected_int8_42):
     df = pl.Series([42], dtype=pl.Int8).to_frame("test")
     result = df.select(plh.oldhash(pl.col("test")))
     assert_frame_equal(result, expected_int8_42.to_frame())
-
 
 
 def test_int_struct(raw_struct_df, expected_int_struct):
@@ -38,7 +38,6 @@ def test_int_struct_singular(raw_struct_df_singular, expected_int_struct_singula
     print(result)
 
     assert_frame_equal(result, expected_int_struct_singular.to_frame())
-
 
 
 def test_int_dtype_struct(int_dtype_struct, expected_int_dtype_struct):
